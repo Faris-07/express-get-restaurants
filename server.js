d1b6882287e09d58f547ebe9a830c7c84fbb3fe6
Faris-07 <farisuddin03@gmail.com>
2023-02-23 17:02:00 +0000

    Complete Part 4

--- server.js
+++ server.js
@@ -2,46 +2,12 @@ const express = require("express");
 const app = express();
 const {Restaurant} = require("./models/index")
 const {sequelize} = require("./db");
+const restaurantsRouter = require("./routers/Restaurant");
 
 const port = 3000;
+
 app.use(express.json())
-
-//TODO: Create your GET Request Route Below: 
-
-app.get('/restaurants', async (req, res) => {
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-});
-
-app.get('/restaurants/:id', async (req, res) => {
-    const restaurant = await Restaurant.findByPk(req.params.id);
-    res.json(restaurant)
-})
-
-
-app.post('/restaurants', async (req, res) => {
-    req.body = {name: "Something", location: "Somewhere", cuisine: "SomeWho"}
-    const newRestaurant = req.body;
-    await Restaurant.create(newRestaurant);
-    const allRestaurants = await Restaurant.findAll()
-    res.json(allRestaurants);
-})
-
-app.put('/restaurants/:id', async (req, res) => {
-    let id = req.params.id;
-    req.body = {name: "Something", location: "Somewhere", cuisine: "SomeWho"}
-    const newRestaurant = req.body;
-    await Restaurant.update(newRestaurant, { where: { id: id } });
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-})
-
-app.delete('/restaurants/:id', async (req, res) => {
-    let id = req.params.id;
-    await Restaurant.destroy({ where: { id: id } });
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-})
+app.use("/restaurant", restaurantsRouter);
 
 app.listen(port, () => {
     sequelize.sync();
